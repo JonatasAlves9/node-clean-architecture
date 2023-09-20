fde4c12ec702064a2f90d9797a6e6eaa09bc0465
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-09-20 13:50:24 -0300

    add facebook authentication feature

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,19 @@
 module.exports = {
+
   overrides: [
     {
       files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
-      extends: 'standard-with-typescript'
-    }
+      extends: 'standard-with-typescript',
+      rules: {
+        "@typescript-eslint/no-namespace": "off"
+      },
+    },
   ],
+
   parserOptions: {
     parser: '@typescript-eslint/parser',
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
   },
+
 }
