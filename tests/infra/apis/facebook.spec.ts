46f1d91c5239316c1f941a15302ad032cc663b8a
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-09-22 12:04:43 -0300

    ensure FacebookApi get debug token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,6 +1,7 @@
 import { mock, type MockProxy } from 'jest-mock-extended'
 import { type HttpGetClient } from '@/infra/http'
 import { FacebookApi } from '@/infra/apis'
+import * as http from 'http'
 
 describe('FacebookApi', () => {
   let clientId: string
@@ -15,6 +16,9 @@ describe('FacebookApi', () => {
   })
 
   beforeEach(() => {
+    httpClient.get.mockResolvedValueOnce({
+      access_token: 'any_app_token'
+    })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -29,6 +33,20 @@ describe('FacebookApi', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grantType: 'client_credenctial'
+      }
+    })
+  })
+
+  it('should get debug token', async () => {
+    await sut.loadUser({
+      token: 'any_client_token'
+    })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   })
