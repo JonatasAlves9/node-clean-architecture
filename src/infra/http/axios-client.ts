dc6bf57ff5f018dab7fab980621e9f61490b29fd
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-09-22 16:40:48 -0300

    make implements in AxiosClient and jwtTokenGenerator

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -1,7 +1,7 @@
 import { type HttpGetClient } from '@/infra/http/client'
 import axios from 'axios'
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements HttpGetClient {
   async get<T = any>(args: HttpGetClient.Params): Promise<T> {
     const result = await axios.get<T>(args.url, {
       params: args.params
