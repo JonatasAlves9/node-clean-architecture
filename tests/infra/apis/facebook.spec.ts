12be6e708eaaba30eb85168d4383be00a8e7f48a
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-09-22 11:49:45 -0300

    move production code to their own files

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,38 +1,6 @@
-import { type LoadFacebookUserApi } from '@/data/contracts/apis'
 import { mock, type MockProxy } from 'jest-mock-extended'
-
-class FacebookApi {
-  private readonly baseUrl = 'https://graph.facebook.com'
-
-  constructor (
-    private readonly httpClient: HttpGetClient,
-    private readonly clientId: string,
-    private readonly clientSecret: string
-  ) {
-  }
-
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
-    await this.httpClient.get({
-      url: `${this.baseUrl}/oauth/access_token`,
-      params: {
-        client_id: this.clientId,
-        client_secret: this.clientSecret,
-        grantType: 'client_credenctial'
-      }
-    })
-  }
-}
-
-interface HttpGetClient {
-  get: (params: HttpGetClient.Params) => Promise<void>
-}
-
-namespace HttpGetClient {
-  export interface Params {
-    url: string
-    params: object
-  }
-}
+import { type HttpGetClient } from '@/infra/http'
+import { FacebookApi } from '@/infra/apis'
 
 describe('FacebookApi', () => {
   let clientId: string
