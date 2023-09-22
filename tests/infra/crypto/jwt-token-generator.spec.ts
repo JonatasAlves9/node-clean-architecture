13d665cd6de1ac3fe9b62600a104b95ec0b68ff4
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-09-22 14:23:00 -0300

    ensure JwtTokenGenerator rethrows if sign throw

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -36,4 +36,17 @@ describe('JwtTokenGenerator', () => {
 
     expect(token).toBe('any_token')
   })
+
+  it('should rethrow if sign throws', async () => {
+    fakeJwt.sign.mockImplementation(() => {
+      throw new Error('token_error')
+    })
+
+    const promise = sut.generateToken({
+      key,
+      expirationInMs: 1000
+    })
+
+    await expect(promise).rejects.toThrow(new Error('token_error'))
+  })
 })
