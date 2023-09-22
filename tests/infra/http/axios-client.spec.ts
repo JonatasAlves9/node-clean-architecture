41580b896ab73cb6a885ee8cb94ebaf0b25fc2ef
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-09-22 13:22:59 -0300

    ensure AxiosHttpClient calls axios.get with correct params

---
+++ tests/infra/http/axios-client.spec.ts
@@ -0,0 +1,36 @@
+import { type HttpGetClient } from '@/infra/http'
+import axios from 'axios'
+
+jest.mock('axios')
+
+class AxiosHttpClient {
+  async get (args: HttpGetClient.Params): Promise<void> {
+    await axios.get(args.url, {
+      params: args.params
+    })
+  }
+}
+
+describe('AxiosHttp', () => {
+  describe('get', () => {
+    it('should call get with correct params', async () => {
+      const sut = new AxiosHttpClient()
+      const fakeAxios = axios as jest.Mocked<typeof axios>
+
+      await sut.get({
+        url: 'any_url',
+        params: {
+          any: 'any'
+        }
+      })
+
+      expect(fakeAxios.get).toHaveBeenCalledWith('any_url',
+        {
+          params: {
+            any: 'any'
+          }
+        })
+      expect(fakeAxios.get).toHaveBeenCalledTimes(1)
+    })
+  })
+})
