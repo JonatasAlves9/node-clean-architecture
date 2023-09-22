fe2a7d63d2bf0e35a5bf36eaf9e51b313fc632f7
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-09-21 21:10:28 -0300

    ensure AccessToken expire in 30 minutes

--- tests/main/models/access-token.spec.ts
+++ tests/main/models/access-token.spec.ts
@@ -8,4 +8,8 @@ describe('Access Token', () => {
       value: 'any_value'
     })
   })
+
+  it('should expire in 1800000 ms', () => {
+    expect(AccessToken.expirationInMs).toBe(1800000)
+  })
 })
