3addb9c734e6fac27475144a2549316fde3c60ad
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-09-23 16:30:14 -0300

    user generics instead of any

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,13 +1,21 @@
 import { type FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
 import { badRequest, type HttpResponse, ok, serverError, unauthorized } from '@/application/helpers'
-import { RequiredFieldError, ServerError } from '@/application/errors'
+import { RequiredFieldError } from '@/application/errors'
+
+interface HttpRequest {
+  token: string | undefined | null
+}
+
+type Model = Error | {
+  accessToken: string
+}
 
 export class FacebookLoginController {
   constructor (private readonly facebookAuthentication: FacebookAuthentication) {
   }
 
-  async handle (httpRequest: any): Promise<HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
       if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
         return badRequest(new RequiredFieldError('token'))
