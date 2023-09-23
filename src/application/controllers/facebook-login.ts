c895498e96ee0cc35683c3b716a790e3c1ace62e
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-09-23 16:56:58 -0300

    make FacebookLoginController use RequiredStringValitadot

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -2,6 +2,7 @@ import { type FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
 import { badRequest, type HttpResponse, ok, serverError, unauthorized } from '@/application/helpers'
 import { RequiredFieldError } from '@/application/errors'
+import { RequiredStringValidator } from '@/application/validation'
 
 interface HttpRequest {
   token: string
@@ -38,8 +39,7 @@ export class FacebookLoginController {
   }
 
   validate = (httpRequest: HttpRequest): Error | undefined => {
-    if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
-      return new RequiredFieldError('token')
-    }
+    const validator = new RequiredStringValidator(httpRequest.token, 'token')
+    return validator.validate()
   }
 }
